ee81812d8d64ed573db228d84cf08590b0cc6575
Nilson <nilsonrx@gmail.com>
2021-11-22 22:25:01 -0300

    Projeto Games

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+import NavBar from '../NavBar/NavBar'
+
+export default function Main () {
+    return (
+    <div>
+        <NavBar/>
+        <h1>Projeto</h1>
+    </div>
+    )
+}
